8f4188e14d302b377a8a4e626c9ec3c79be60141
erick garcia <erickgarcia@paypal.com>
2021-11-17 13:11:09 -0500

    switch logo and show label animation, fix issues with responsive

--- src/ui/buttons/button-animations/switch-logo-and-show-label-text.jsx
+++ src/ui/buttons/button-animations/switch-logo-and-show-label-text.jsx
@@ -24,6 +24,17 @@ export function AnimationComponent({ animationLabelText, logoColor } : LabelOpti
         <div class={ config.labelClass } data-animation-experiment={ config.experimentName }>
             <PPLogo logoColor={ logoColor } />
             <span>{config.labelText}</span>
+            <style innerHTML={ `
+                .${ CLASS.DOM_READY } .${ ANIMATION.CONTAINER } img.${ LOGO_CLASS.LOGO }-pp{
+                    position: relative;
+                    opacity:0;
+                }
+                .${ ANIMATION.CONTAINER } .${ ANIMATION.LABEL_CONTAINER } {
+                    opacity: 0; 
+                    color: #142C8E;
+                    font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+                }
+            ` } />
         </div>
     );
 }
@@ -31,7 +42,7 @@ export function AnimationComponent({ animationLabelText, logoColor } : LabelOpti
 // Returns label container if the button sizes match
 const getAnimationProps = function(document, configuration) : AnimationProps | null {
     let labelFontSize = 8;
-    const { ANIMATION_CONTAINER, ANIMATION_LABEL_CONTAINER, PAYPAL_BUTTON_LABEL, PAYPAL_LOGO } = configuration.cssClasses;
+    const { ANIMATION_CONTAINER, ANIMATION_LABEL_CONTAINER, PAYPAL_BUTTON_LABEL } = configuration.cssClasses;
     const { tiny, small, medium } = configuration;
     // get the animation main container to force specificity( in css ) and make sure we are running the right animation
     const animationContainer = (document && document.querySelector(`.${ ANIMATION_CONTAINER }`)) || null;
@@ -41,6 +52,7 @@ const getAnimationProps = function(document, configuration) : AnimationProps | n
 
     // return null if animation should not be played for the button size
     const animationContainerWidth = animationContainer.offsetWidth;
+    
     if (animationContainerWidth < tiny.min || animationContainerWidth >= medium.max) {
         // remove label element from dom
         animationContainer.querySelector(`.${ ANIMATION_LABEL_CONTAINER }`).remove();
@@ -53,23 +65,15 @@ const getAnimationProps = function(document, configuration) : AnimationProps | n
 
     // get the label container that animation will be applied to
     const paypalLabelContainerElement = animationContainer.querySelector(`.${ PAYPAL_BUTTON_LABEL }`) || null;
-    // get starting position for element so it doesn't jump when animation begins
-    const paypalLogoElement = (paypalLabelContainerElement && paypalLabelContainerElement.querySelector(`.${ PAYPAL_LOGO }`)) || null;
-    const paypalLogoStartingLeftPosition = paypalLogoElement
-        ? `${ (paypalLogoElement.offsetLeft / paypalLabelContainerElement.offsetWidth) * 100 }`
-        : '44.5';
-
     return {
         labelFontSize,
-        paypalLabelContainerElement,
-        paypalLogoStartingLeftPosition
+        paypalLabelContainerElement
     };
 };
 
 const createAnimation = function (animationProps, cssClasses) : void | null {
     const { ANIMATION_LABEL_CONTAINER, ANIMATION_CONTAINER, DOM_READY, PAYPAL_LOGO } = cssClasses;
-    const { paypalLabelContainerElement, paypalLogoStartingLeftPosition, labelFontSize } = animationProps;
-    const logoStartingPosition = parseInt(paypalLogoStartingLeftPosition, 10) + 4;
+    const { paypalLabelContainerElement, labelFontSize } = animationProps;
     const animations = `
         .${ DOM_READY } .${ ANIMATION_CONTAINER } img.${ PAYPAL_LOGO }-paypal{
             animation: 4s move-logo-to-left-side 0.5s infinite alternate;
@@ -79,9 +83,6 @@ const createAnimation = function (animationProps, cssClasses) : void | null {
 
         .${ ANIMATION_CONTAINER } .${ ANIMATION_LABEL_CONTAINER } {
             position: fixed;
-            opacity: 0; 
-            color: #142C8E;
-            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
             animation: 4s divide-logo-animation-right-side 0.5s infinite alternate;
             text-align: center;
             width: 100%;
@@ -90,20 +91,18 @@ const createAnimation = function (animationProps, cssClasses) : void | null {
         }
 
         .${ DOM_READY } .${ ANIMATION.CONTAINER } img.${ LOGO_CLASS.LOGO }-pp{
-            position: relative;
             animation: 4s move-small-paypal 0.5s infinite alternate;
-            opacity:0;
-            padding-right: 2px;
+            padding-right: 4px;
         }
 
         @keyframes move-logo-to-left-side {
             0%,33% {
-                transform:translateX(-50%);
+                transform: translateX(-50%);
                 opacity:1;
             }
             50%,100% {
                 position:fixed;
-                transform:translateX(-94%);
+                transform: translateX(-94%);
                 opacity:0;
             }
         }
@@ -150,7 +149,7 @@ export function setupSwitchLogoAndShowLabelTextAnimation (animationLabelText : S
     };
     const buttonAnimationScript = `
         const animationProps = ${ getAnimationProps.toString() }( document, ${ JSON.stringify(animationConfig) });
-        if (animationProps && animationProps.paypalLabelContainerElement && animationProps.paypalLogoStartingLeftPosition) {
+        if (animationProps) {
             const animation = ${ createAnimation.toString() }
             animation(animationProps, ${ JSON.stringify(animationConfig.cssClasses) })
         }
